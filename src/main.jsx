4dab97dc00e1f4b6382e7ec625282cefe20975da
estebanb <bravo.esteban@hotmail.com>
2025-04-07 12:12:25 -0400

    segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { GifExpertApp } from './GifExpertApp'
 
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GifExpertApp/>
